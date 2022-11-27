ef5c96a4da6abcd1ac264ad2c802ed5a18d28bbd
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-27 15:46:42 -0600

    added inquirer, path and fs in index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
