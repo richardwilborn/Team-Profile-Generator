d1a62dc45f9434c4413b7eef581ff38f9d69c865
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-30 13:09:31 -0600

    added code for tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,20 @@
 const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+test("Can set phone number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Abc", 1, "test@test.com", testValue);
+  expect(e.phoneNumber).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Abc", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get phone number via getPhoneNumber()", () => {
+  const testValue = 100;
+  const e = new Manager("Abc", 1, "test@test.com", testValue);
+  expect(e.getPhoneNumber()).toBe(testValue);
+});
